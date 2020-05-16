50e956adc78beddc5747434350932c991e470fdf
Hansin Susatya <hansinsusatya@gmail.com>
2020-05-16 10:19:44 +0700

    Load orders

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -6,6 +6,50 @@ import { OrderService } from '@services';
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @Get()
+  async loadOrders(@Req() req: Request, @Res() res: Response) {
+    try {
+      let payload;
+      payload = {
+        token: req.headers.authorization.split(' ')[1],
+      };
+      const date = req.query.date;
+      const shipping = req.query.shipping;
+      const status = req.query.status;
+      if (date) {
+        payload.date = date;
+      }
+      if (shipping) {
+        payload.shipping = shipping;
+      }
+      if (status) {
+        payload.status = status;
+      }
+      const response = await this.orderService.loadOrders(payload);
+      res.status(response.status).json(response);
+    } catch (error) {
+      throw new HttpException(error, error.status);
+    }
+  }
+
+  @Get('total')
+  async loadTotalOrderTotalPage(@Req() req: Request, @Res() res: Response) {
+    try {
+      let payload;
+      payload = {
+        token: req.headers.authorization.split(' ')[1],
+      };
+      const date = req.query.date;
+      if (date) {
+        payload.date = date;
+      }
+      const response = await this.orderService.loadTotalOrderTotalPage(payload);
+      res.status(response.status).json(response);
+    } catch (error) {
+      throw new HttpException(error, error.status);
+    }
+  }
+
   @Get('shipping')
   async loadOrderShippings(@Req() req: Request, @Res() res: Response) {
     try {
@@ -41,4 +85,23 @@ export class OrderController {
       throw new HttpException(error, error.status);
     }
   }
+
+  @Get('locked')
+  async loadOrderLocked(@Req() req: Request, @Res() res: Response) {
+    try {
+      let payload;
+      payload = {
+        token: req.headers.authorization.split(' ')[1],
+      };
+      const date = req.query.date;
+      if (date) {
+        payload.date = date;
+      }
+      const response = await this.orderService.loadOrderLocked(payload);
+      res.status(response.status).json(response);
+    } catch (error) {
+      throw new HttpException(error, error.status);
+    }
+  }
+
 }
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -6,6 +6,16 @@ import { IPayload } from '@interfaces';
 export class OrderService {
   constructor(@Inject('ORDER_SERVICE') private readonly clientService: ClientProxy) {}
 
+  loadOrders(payload: IPayload) {
+    const pattern = { cmd: 'orders'};
+    return this.clientService.send<any>(pattern, payload).toPromise();
+  }
+
+  loadTotalOrderTotalPage(payload: IPayload) {
+    const pattern = { cmd: 'order-total'};
+    return this.clientService.send<any>(pattern, payload).toPromise();
+  }
+
   loadOrderShippings(payload: IPayload) {
     const pattern = { cmd: 'order-shippings'};
     return this.clientService.send<any>(pattern, payload).toPromise();
@@ -13,6 +23,11 @@ export class OrderService {
 
   loadOrderStatus(payload: IPayload) {
     const pattern = { cmd: 'order-status'};
+    return this.clientService.send<any>(pattern, payload).toPromise();
+  }
+
+  loadOrderLocked(payload: IPayload) {
+    const pattern = { cmd: 'order-locked'};
     return this.clientService.send<any>(pattern, payload).toPromise();
   }
 
