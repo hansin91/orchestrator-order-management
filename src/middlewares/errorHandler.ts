88242415fb3317499ba275c669e1a0f98eed5455
hansin91 <hansinsusatya@gmail.com>
2022-08-31 21:06:51 +0700

    remove semicolons

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,15 +3,15 @@ import {
   Catch,
   ArgumentsHost,
   HttpStatus,
-} from '@nestjs/common';
+} from '@nestjs/common'
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-    const status = exception.response.error.status || HttpStatus.INTERNAL_SERVER_ERROR;
-    const message = exception.response.error.message || 'Internal Server Error';
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse()
+    const status = exception.response.error.status || HttpStatus.INTERNAL_SERVER_ERROR
+    const message = exception.response.error.message || 'Internal Server Error'
 
     response.status(status).json({
       status,
